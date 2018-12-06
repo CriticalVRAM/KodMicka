dad89614def02006765b29e299928cfc776c16b2
Nani-Sore-0 <urke.radule@gmail.com>
2018-12-06 15:37:53 +0100

    about

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,9 @@ require('slick-carousel')
 
 
 $(document).ready(function () {
-  $('.about__slick').slick()
+  $('.about__slick').slick({
+    fade: true,
+    prevArrow: '<svg class="about__icon about__icon--prev"><use href="assets/components/sprites.svg#icon-arrow-left2">',
+    nextArrow: '<svg class="about__icon about__icon--next"><use href="assets/components/sprites.svg#icon-arrow-right2">',
+  })
 })
