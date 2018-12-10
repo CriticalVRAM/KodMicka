f2bf6df5aaa899df8508719bba51a96fbfc0d0da
Nani-Sore-0 <urke.radule@gmail.com>
2018-12-10 16:59:40 +0100

    work in ie11 :star:

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 require('slick-carousel')
 import AOS from 'aos'
 import SmoothScroll from "smooth-scroll"
+import svg4everybody from 'svg4everybody'
 
 
 $(document).ready(function () {
@@ -9,10 +10,12 @@ $(document).ready(function () {
     prevArrow: '<svg class="about__icon about__icon--prev"><use href="assets/components/sprites.svg#icon-arrow-left2">',
     nextArrow: '<svg class="about__icon about__icon--next"><use href="assets/components/sprites.svg#icon-arrow-right2">',
   })
-})
+  
+  new SmoothScroll('a[href*="#"]')
 
-new SmoothScroll('a[href*="#"]')
+  AOS.init({
+    duration: 1000
+  })
 
-AOS.init({
-  duration: 1000
+  svg4everybody()
 })
